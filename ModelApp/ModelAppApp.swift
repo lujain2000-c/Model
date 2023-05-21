8120559b61335f5c312a8337b06d41458295dc03
LUJAIN ALKANHAL <lujain.i.k20@gmail.com>
2023-05-21 21:29:20 +0300

    Initial Commit

---
+++ ModelApp/ModelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelAppApp.swift
+//  ModelApp
+//
+//  Created by لجين إبراهيم الكنهل on 01/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ModelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
